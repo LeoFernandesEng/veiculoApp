0ff715684be87b5ecf81cb06479a02bd523b318b
= <=>
2024-07-20 20:10:09 -0300

    initial commit

---
+++ src/test/java/com/app/veiculos_app/VeiculosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.veiculos_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeiculosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ veiculos-app/src/main/java/com/app/veiculos_app/VeiculosAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.veiculos_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeiculosAppApplication.class, args);
+	}
+
+}
